878b6f747824067a952c9c1dcff493968980af8f
Flavio <flaviotps@gmail.com>
2020-09-03 19:08:09 -0300

    ROPE Git Ignore

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//It is common to create a class to contain all of your
+//extension methods. This class must be static.
+public static class Extensions
+{
+    //Even though they are used like normal methods, extension
+    //methods must be declared static. Notice that the first
+    //parameter has the 'this' keyword followed by a Transform
+    //variable. This variable denotes which class the extension
+    //method becomes a part of.
+    public static Vector2 round2d(this Vector2 vector)
+    {
+        vector.x = (float) Math.Round(vector.x, 1);
+        vector.y = (float) Math.Round(vector.y, 1);
+        return vector;
+    }
+    
+    public static Vector2 round2d(this Vector3 vector)
+    {
+        vector.x = (float) Math.Round(vector.x, 1);
+        vector.y = (float) Math.Round(vector.y, 1);
+        vector.z = 0;
+        return vector;
+    }
+    
+    public static T last<T>(this IList<T> list)
+    {
+        if (list == null)
+            throw new ArgumentNullException("list");
+        if (list.Count == 0)
+            throw new ArgumentException(
+                "Cannot get last item because the list is empty");
+
+        int lastIdx = list.Count - 1;
+        return list[lastIdx];
+    }
+    
+    public static T get<T>(this IList<T> list, int index)
+    {
+        if (list == null)
+            throw new ArgumentNullException("list");
+        if (list.Count == 0)
+            throw new ArgumentException(
+                "Cannot get last item because the list is empty");
+
+        return list[index];
+    }
+    
+    public static T getAtEnd<T>(this IList<T> list, int index)
+    {
+        if (list == null)
+            throw new ArgumentNullException("list");
+        if (list.Count == 0)
+            throw new ArgumentException(
+                "Cannot get last item because the list is empty");
+
+        return list[list.Count - index];
+    }
+    
+    public static int lastIndex<T>(this IList<T> list)
+    {
+        if (list == null)
+            throw new ArgumentNullException("list");
+        if (list.Count == 0)
+            throw new ArgumentException(
+                "Cannot get last item because the list is empty");
+
+        return list.Count - 1;
+    }
+    
+    
+    public static float round(this float value, int decimalCount)
+    {
+        return (float) Math.Round(value, decimalCount);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] public Rigidbody2D rigidbody2D;
+    [SerializeField] public DistanceJoint2D distanceJoint2D;
+    [SerializeField] public int moveSpeed = 10;
+    [SerializeField] public int climbSpeed = 100;
+    [SerializeField] public int impulseForce = 1000;
+    [SerializeField] public int jumpForce = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void swingRight()
+    {
+        rigidbody2D.AddForce(transform.right * (impulseForce * Time.deltaTime), ForceMode2D.Impulse); 
+    }
+    
+    public void swingLeft()
+    {
+        rigidbody2D.AddForce(transform.right * (-impulseForce * Time.deltaTime), ForceMode2D.Impulse); 
+    }
+    
+    public void moveRight()
+    {
+        transform.position += new Vector3(1*moveSpeed * Time.deltaTime,0,0);
+    }
+    
+    public void moveLeft()
+    {
+        transform.position += new Vector3(-1*moveSpeed * Time.deltaTime,0,0);
+    }
+    
+    public void climbUp()
+    {
+        distanceJoint2D.distance -= climbSpeed * Time.deltaTime;
+        if (distanceJoint2D.distance < 0)
+        {
+            distanceJoint2D.distance = 0;
+        }
+    }
+    
+    public void climbDown()
+    {
+        distanceJoint2D.distance += climbSpeed * Time.deltaTime;
+    }
+    
+    public void jump()
+    {
+        rigidbody2D.AddForce(transform.up * (jumpForce * Time.deltaTime), ForceMode2D.Impulse); 
+    }
+}
